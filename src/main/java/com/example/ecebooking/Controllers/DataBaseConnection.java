a8d0a6f1f49007e45b74b7afeeaf351be2fff442
apollinelgnc <leguenicapolline@gmail.com>
2023-04-04 00:00:04 +0200

    modif DataBaseConnection

--- src/main/java/com/example/ecebooking/Controllers/DataBaseConnection.java
+++ src/main/java/com/example/ecebooking/Controllers/DataBaseConnection.java
@@ -1,12 +1,10 @@
 package com.example.ecebooking.Controllers;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 
 public class DataBaseConnection {
     public Connection databaseLink;
-    public Connection getConnection(){
+    public Connection getConnection() throws SQLException {
         String databaseName="login";
         String databaseUser="root";
         String databasePassword="0802";
@@ -16,6 +14,14 @@ public class DataBaseConnection {
         }
         catch (Exception e) {
             e.printStackTrace();
+        }
+        Statement stmt = databaseLink.createStatement();
+        ResultSet res=stmt.executeQuery("SELECT * FROM login.user_client");
+        while(res.next()){
+            System.out.println(res.getInt("User_client"));
+            System.out.println(res.getString("name_client"));
+            System.out.println(res.getString("idUser_client"));
+            System.out.println(res.getString("mdptUse_client"));
         }
         return databaseLink;
 

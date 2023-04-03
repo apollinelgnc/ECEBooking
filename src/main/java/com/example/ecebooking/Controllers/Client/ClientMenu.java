11f23444c7a7d97564075e52e88ea0b8418389e0
apollinelgnc <leguenicapolline@gmail.com>
2023-04-03 10:43:36 +0200

    loggin essai 3

--- src/main/java/com/example/ecebooking/Controllers/Client/ClientMenu.java
+++ src/main/java/com/example/ecebooking/Controllers/Client/ClientMenu.java
@@ -2,6 +2,8 @@ package com.example.ecebooking.Controllers.Client;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,5 +13,9 @@ public class ClientMenu implements Initializable {
     public Button stats_button;
     public Button profile_button;
     public Button log_out_button;
+    public Menu hotels_menu_bar;
+    public Menu compte_menu_bar;
+    public Menu help_menu_bar;
+
     public void initialize(URL url, ResourceBundle resourceBundle){}
 }
---
+++ src/main/java/com/example/ecebooking/Controllers/Client/MenuController.java
@@ -0,0 +1,10 @@
+package com.example.ecebooking.Controllers.Client;
+
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class MenuController {
+    public TextField nombrePersonnesField;
+    public DatePicker dateArriveePicker;
+    public DatePicker dateDepartPicker;
+}
